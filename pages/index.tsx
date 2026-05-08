5f185c91888caab38640125718cf051fbadc6d51
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2026-05-09 00:39:56 +0530

    changes

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Certificate Generation App</h1>
+            {/* Add navigation or content here */}
+        </div>
+    );
+};
+
+export default Home;
